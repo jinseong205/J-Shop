6a47eff94e47874ed30d3b8074966a0f79dcb036
jinseong <jinseong205@gmail.com>
2023-09-07 00:58:51 +0900

    feat: Spring Security 설정 및 user 관련 기능 구현
    - oracle adw 관련 라이브러리 추가
    - spring security 설정
    - jwt 인증 / 인가 필터 설정
    - user 생성, 수정, 삭제, 조회 기능 추가
    - audit를 이용한 생성자/수정자 생성시간/수정시간 추적
    - custom Excpetion 추가 --> 추후 변경 예정

---
+++ server/src/main/java/com/shop/server/dto/UserFormDto.java
@@ -0,0 +1,31 @@
+package com.shop.server.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+
+@Data						//Getter Setter
+public class UserFormDto {
+	/* 회원 가입 전용 DTO (req) */
+	
+	@NotBlank(message = "아이디를 입력해주세요.")
+	@Length(min=8, max=20, message = "아이디는 8자 이상 20자 이하로 입력하세요.")
+	private String username;	
+	
+	@NotBlank(message = "비밀번호는를 입려해주세요.")
+	@Length(min=8, max=20, message = "패스워드는 8자 이상 20자 이하로 입력하세요.")
+
+	private String password;	
+	
+	private String name;
+	
+	private String email;
+	
+	@NotBlank(message = "주소를 입력해주세요.")
+	private String addr;
+	
+
+}
+	
---
+++ server/src/main/java/com/shop/server/error/ApiExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.shop.server.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+// Target all Controllers annotated with @RestController
+@ControllerAdvice(annotations = RestController.class)
+@RestController
+@Slf4j
+public class ApiExceptionHandler {
+
+	@ExceptionHandler
+	public ResponseEntity<?> commonExceptionHandler(Exception e) {
+		
+		log.error("[ERROR]" , e);
+		
+		String errorMessage;
+		
+		if (e instanceof MissingServletRequestPartException) {
+			errorMessage = "입력정보가 누락되었습니다.\n" + e.getMessage();
+			return new ResponseEntity<>( new ErrorResult(errorMessage), HttpStatus.BAD_REQUEST);
+		}else {
+			return new ResponseEntity<>( new ErrorResult(e.getMessage()), HttpStatus.BAD_REQUEST);
+		}
+		
+
+
+	}
+}
