bed2fda660b822e384be18ae9885d987a95d6c60
jinseong <jinseong205@gmail.com>
2023-09-14 00:23:56 +0900

    feat: User 업무 단위 테스트 개발
    - User 관련 업무 단위 테스트 개발

---
+++ server/src/test/java/com/shop/server/service/UserServiceTest.java
@@ -0,0 +1,55 @@
+package com.shop.server.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.shop.server.dto.UserFormDto;
+import com.shop.server.entity.User;
+
+import jakarta.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+public class UserServiceTest {
+
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	public User createUser() {
+		UserFormDto userDto = new UserFormDto();
+		
+		userDto.setUsername("gomawoomi");
+		userDto.setPassword("0000");
+		userDto.setName("정진성");
+		userDto.setEmail("goamwoomi@gmail.com");;
+		
+		return User.builder()
+				.username(userDto.getUsername())
+				.password(bCryptPasswordEncoder.encode(userDto.getPassword()))
+				.name(userDto.getName())
+				.email(userDto.getEmail())
+				.roles("ROLE_USER")
+				.build();	
+	}
+	
+	@Test
+	@DisplayName("--- 회원가입 테스트 ---")
+	public void saveMemberTest() {
+		
+		User user = createUser();
+		User savedUser = userService.join(user);
+
+		assertEquals(user.getUsername(), savedUser.getUsername());
+		assertEquals(user.getPassword(), savedUser.getPassword());
+		assertEquals(user.getName(), savedUser.getName());
+		assertEquals(user.getEmail(), savedUser.getEmail());
+	}
+}
